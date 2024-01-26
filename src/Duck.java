a4d40ec3e43e856caea77ff2e774421f55514e9a
shahid531 <shahidansari531@gmail.com>
2024-01-26 22:38:34 +0530

    added duck functionality

--- src/Duck.java
+++ src/Duck.java
@@ -1,16 +1,16 @@
-public class Duck implements Features,Fly{
+public class Duck implements Features, Fly {
     @Override
     public void walk() {
-
+        System.out.println("walking");
     }
 
     @Override
     public void run() {
-
+        System.out.println("running");
     }
 
     @Override
     public void fly() {
-
+        System.out.println("flying");
     }
 }
